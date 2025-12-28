7cffa2caea87a45edcfc7ee3b18b728bfbae9bc5
Bela Kampis <belakampis@gmail.com>
2025-12-28 13:51:54 -0500

    From Monado

--- direct_usb_gazes/PSVR2_DirectGazeReader/PSVR2_DirectGazeReader.cpp
+++ direct_usb_gazes/PSVR2_DirectGazeReader/PSVR2_DirectGazeReader.cpp
@@ -239,6 +239,7 @@ static int test_wrapped_device(const char* device_name)
 	return 1;
 }
 
+#if 0
 bool psvr2_usb_start(psvr2_hmd* hmd)
 {
 	bool result = false;
@@ -246,19 +247,22 @@ bool psvr2_usb_start(psvr2_hmd* hmd)
 
 	os_thread_helper_lock(&hmd->usb_thread);
 
-	/* Status endpoint */
+	// Status endpoint
 	hmd->status_xfer = libusb_alloc_transfer(0);
+
 	if(hmd->status_xfer == NULL)
 	{
 		PSVR2_ERROR(hmd, "Could not alloc USB transfer for status reports");
 		goto out;
 	}
 	uint8_t* status_buf = malloc(USB_STATUS_XFER_SIZE);
-	libusb_fill_interrupt_transfer(hmd->status_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_STATUS_ENDPOINT,
-		status_buf, USB_STATUS_XFER_SIZE, status_xfer_cb, hmd, 0);
+	
+	libusb_fill_interrupt_transfer(hmd->status_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_STATUS_ENDPOINT, status_buf, USB_STATUS_XFER_SIZE, status_xfer_cb, hmd, 0);
+
 	hmd->status_xfer->flags |= LIBUSB_TRANSFER_FREE_BUFFER;
 
 	res = libusb_submit_transfer(hmd->status_xfer);
+
 	if(res < 0)
 	{
 		PSVR2_ERROR(hmd, "Could not submit USB transfer for status reports");
@@ -266,7 +270,7 @@ bool psvr2_usb_start(psvr2_hmd* hmd)
 	}
 	hmd->usb_active_xfers++;
 
-	/* Camera data */
+	// Camera data
 	hmd->camera_enable = true;
 	hmd->camera_mode = PSVR2_CAMERA_MODE_10;
 	set_camera_mode(hmd, hmd->camera_mode);
@@ -282,8 +286,7 @@ bool psvr2_usb_start(psvr2_hmd* hmd)
 
 		uint8_t* recv_buf = malloc(USB_CAM_MODE10_XFER_SIZE);
 
-		libusb_fill_bulk_transfer(hmd->camera_xfers[i], hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_CAMERA_ENDPOINT,
-			recv_buf, USB_CAM_MODE10_XFER_SIZE, img_xfer_cb, hmd, 0);
+		libusb_fill_bulk_transfer(hmd->camera_xfers[i], hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_CAMERA_ENDPOINT, recv_buf, USB_CAM_MODE10_XFER_SIZE, img_xfer_cb, hmd, 0);
 		hmd->camera_xfers[i]->flags |= LIBUSB_TRANSFER_FREE_BUFFER;
 
 		res = libusb_submit_transfer(hmd->camera_xfers[i]);
@@ -295,19 +298,23 @@ bool psvr2_usb_start(psvr2_hmd* hmd)
 		hmd->usb_active_xfers++;
 	}
 
-	/* SLAM endpoint */
+	// SLAM endpoint
 	hmd->slam_xfer = libusb_alloc_transfer(0);
+
 	if(hmd->slam_xfer == NULL)
 	{
 		PSVR2_ERROR(hmd, "Could not alloc USB transfer for SLAM data");
 		goto out;
 	}
+
 	uint8_t* slam_buf = malloc(USB_SLAM_XFER_SIZE);
-	libusb_fill_bulk_transfer(hmd->slam_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_SLAM_ENDPOINT, slam_buf,
-		USB_SLAM_XFER_SIZE, slam_xfer_cb, hmd, 0);
+
+	libusb_fill_bulk_transfer(hmd->slam_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_SLAM_ENDPOINT, slam_buf, USB_SLAM_XFER_SIZE, slam_xfer_cb, hmd, 0);
+
 	hmd->slam_xfer->flags |= LIBUSB_TRANSFER_FREE_BUFFER;
 
 	res = libusb_submit_transfer(hmd->slam_xfer);
+
 	if(res < 0)
 	{
 		PSVR2_ERROR(hmd, "Could not submit USB transfer for SLAM data");
@@ -315,19 +322,23 @@ bool psvr2_usb_start(psvr2_hmd* hmd)
 	}
 	hmd->usb_active_xfers++;
 
-	/* LD endpoint */
+	// LD endpoint
 	hmd->led_detector_xfer = libusb_alloc_transfer(0);
+
 	if(hmd->led_detector_xfer == NULL)
 	{
 		PSVR2_ERROR(hmd, "Could not alloc USB transfer for LED Detector data");
 		goto out;
 	}
+
 	uint8_t* led_detector_buf = malloc(USB_LD_XFER_SIZE);
-	libusb_fill_bulk_transfer(hmd->led_detector_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_LD_ENDPOINT,
-		led_detector_buf, USB_LD_XFER_SIZE, dump_xfer_cb, hmd, 0);
+
+	libusb_fill_bulk_transfer(hmd->led_detector_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_LD_ENDPOINT, led_detector_buf, USB_LD_XFER_SIZE, dump_xfer_cb, hmd, 0);
+
 	hmd->led_detector_xfer->flags |= LIBUSB_TRANSFER_FREE_BUFFER;
 
 	res = libusb_submit_transfer(hmd->led_detector_xfer);
+
 	if(res < 0)
 	{
 		PSVR2_ERROR(hmd, "Could not submit USB transfer for LED Detector data");
@@ -335,19 +346,22 @@ bool psvr2_usb_start(psvr2_hmd* hmd)
 	}
 	hmd->usb_active_xfers++;
 
-	/* RP endpoint */
+	// RP endpoint
 	hmd->relocalizer_xfer = libusb_alloc_transfer(0);
+
 	if(hmd->relocalizer_xfer == NULL)
 	{
 		PSVR2_ERROR(hmd, "Could not alloc USB transfer for RP data");
 		goto out;
 	}
 	uint8_t* relocalizer_buf = malloc(USB_RP_XFER_SIZE);
-	libusb_fill_bulk_transfer(hmd->relocalizer_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_RP_ENDPOINT,
-		relocalizer_buf, USB_RP_XFER_SIZE, dump_xfer_cb, hmd, 0);
+	
+	libusb_fill_bulk_transfer(hmd->relocalizer_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_RP_ENDPOINT, relocalizer_buf, USB_RP_XFER_SIZE, dump_xfer_cb, hmd, 0);
+
 	hmd->relocalizer_xfer->flags |= LIBUSB_TRANSFER_FREE_BUFFER;
 
 	res = libusb_submit_transfer(hmd->relocalizer_xfer);
+
 	if(res < 0)
 	{
 		PSVR2_ERROR(hmd, "Could not submit USB transfer for RP data");
@@ -355,19 +369,21 @@ bool psvr2_usb_start(psvr2_hmd* hmd)
 	}
 	hmd->usb_active_xfers++;
 
-	/* VD endpoint */
+	// VD endpoint
 	hmd->vd_xfer = libusb_alloc_transfer(0);
+
 	if(hmd->vd_xfer == NULL)
 	{
 		PSVR2_ERROR(hmd, "Could not alloc USB transfer for VD data");
 		goto out;
 	}
 	uint8_t* vd_buf = malloc(USB_VD_XFER_SIZE);
-	libusb_fill_bulk_transfer(hmd->vd_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_VD_ENDPOINT, vd_buf,
-		USB_VD_XFER_SIZE, dump_xfer_cb, hmd, 0);
+	libusb_fill_bulk_transfer(hmd->vd_xfer, hmd->dev, LIBUSB_ENDPOINT_IN | PSVR2_VD_ENDPOINT, vd_buf, USB_VD_XFER_SIZE, dump_xfer_cb, hmd, 0);
+
 	hmd->vd_xfer->flags |= LIBUSB_TRANSFER_FREE_BUFFER;
 
 	res = libusb_submit_transfer(hmd->vd_xfer);
+
 	if(res < 0)
 	{
 		PSVR2_ERROR(hmd, "Could not submit USB transfer for VD data");
@@ -387,7 +403,11 @@ bool psvr2_usb_start(psvr2_hmd* hmd)
 out:
 	os_thread_helper_unlock(&hmd->usb_thread);
 	return result;
+
+	return 0;
 }
+
+#endif
 
 
 int main(int argc, char* argv[])
--- direct_usb_gazes/PSVR2_DirectGazeReader/psvr2_structs.h
+++ direct_usb_gazes/PSVR2_DirectGazeReader/psvr2_structs.h
@@ -1,6 +1,14 @@
 #ifndef PSVR2_STRUCTS_H
 #define PSVR2_STRUCTS_H
 
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <assert.h>
+#include <libusb.h>
+
+
 #define PSVR2_SLAM_INTERFACE 3
 #define PSVR2_SLAM_ENDPOINT 3
 
@@ -247,5 +255,177 @@ struct psvr2_gaze_state
 
 	psvr2_gaze_packet gaze_data_;
 };
+
+#define NUM_CAM_XFERS 1
+
+#define PSVR2_TRACE(p, ...) U_LOG_XDEV_IFL_T(&p->base, p->log_level, __VA_ARGS__)
+#define PSVR2_TRACE_HEX(p, data, data_size) U_LOG_XDEV_IFL_T_HEX(&p->base, p->log_level, data, data_size)
+#define PSVR2_DEBUG(p, ...) U_LOG_XDEV_IFL_D(&p->base, p->log_level, __VA_ARGS__)
+#define PSVR2_DEBUG_HEX(p, data, data_size) U_LOG_XDEV_IFL_D_HEX(&p->base, p->log_level, data, data_size)
+#define PSVR2_WARN(p, ...) U_LOG_XDEV_IFL_W(&p->base, p->log_level, __VA_ARGS__)
+#define PSVR2_ERROR(p, ...) U_LOG_XDEV_IFL_E(&p->base, p->log_level, __VA_ARGS__)
+
+#define TIMESTAMP_SAMPLES 100
+
+struct psvr2_et_data
+{
+	//struct os_thread_helper eye_tracking_thread;
+
+	//! Whether eye tracking is currently enabled
+	bool want_enabled;
+	bool force_enable;
+
+	//! Whether the eye tracking enable command has been sent
+	bool enabled;
+
+	struct m_relation_history* gaze_relation_history;
+
+	//struct os_mutex data_mutex;
+	psvr2_per_eye_gaze gazes_[2];
+	psvr2_combined_gaze combined_gaze_;
+
+	bool processed_sample_packet;
+
+	uint32_t last_remote_report_sample_time_us;
+	//timepoint_ns last_remote_report_sample_time_ns;
+
+	bool unk_float_4_valid;
+	float unk_float_4;
+
+	bool unk_float_5_valid;
+	float unk_float_5;
+};
+
+struct psvr2_hmd
+{
+	//struct xrt_device base;
+	//struct xrt_pose pose;
+
+	enum u_logging_level log_level;
+
+	//struct os_mutex data_lock;
+
+	/* Device status */
+	uint8_t dprx_status;               //< DisplayPort receiver status
+
+	//xrt_atomic_s32_t proximity_sensor; //< Atomic state for whether the proximity sensor is triggered
+
+	bool function_button;              //< Boolean state for whether the function button is pressed
+
+	bool ipd_updated; //< Whether the IPD has been updated, and an HMD info refresh is needed
+	uint8_t ipd_mm;   //< IPD dial value in mm, from 59 to 72mm
+
+	bool camera_enable;                 //< Whether the camera is enabled
+	enum psvr2_camera_mode camera_mode; //< The current camera mode
+
+	//struct u_var_button camera_enable_btn;
+	//struct u_var_button camera_mode_btn;
+
+	//struct u_var_button brightness_btn;
+	float brightness;
+
+	/* IMU input data */
+	uint32_t last_imu_vts_us;   //< Last VTS timestamp, in microseconds
+	uint16_t last_imu_ts;       //< Last IMU timestamp, in microseconds
+
+	//struct xrt_vec3 last_gyro;  //< Last gyro reading, in rad/s
+	//struct xrt_vec3 last_accel; //< Last accel reading, in m/s²
+
+	/* SLAM input data */
+	uint32_t last_slam_vts_us;      //< Last slam timestamp, in microseconds
+	//struct xrt_pose last_slam_pose; //< Last SLAM pose reading
+
+	//struct xrt_pose slam_correction_pose;
+	//struct u_var_button slam_correction_set_btn;
+	//struct u_var_button slam_correction_reset_btn;
+
+	//struct xrt_pose T_imu_head; //< Constant transform from SLAM tracker pose to head pose
+
+	/* Display parameters */
+	//struct u_device_simple_info info;
+
+	/* Camera debug sinks */
+	//struct u_sink_debug debug_sinks[4];
+
+	/* USB communication */
+	libusb_context* ctx;
+	libusb_device_handle* dev;
+
+	//struct os_thread_helper usb_thread;
+
+	int usb_complete;
+	int usb_active_xfers;
+
+	/* Status report */
+	struct libusb_transfer* status_xfer;
+	/* SLAM (bulk) transfer */
+	struct libusb_transfer* slam_xfer;
+	/* Camera (bulk) transfers */
+	struct libusb_transfer* camera_xfers[NUM_CAM_XFERS];
+	/* LD EP9 (bulk) transfer */
+	struct libusb_transfer* led_detector_xfer;
+	/* RP EP10 (bulk) transfer */
+	struct libusb_transfer* relocalizer_xfer;
+	/* VD EP11 (bulk) transfer */
+	struct libusb_transfer* vd_xfer;
+	/* Gaze transfer */
+	struct libusb_transfer* gaze_xfer;
+
+	/* Distortion calibration parameters, to be used with
+	 * psvr2_compute_distortion_asymmetric. Very specific to
+	 * PS VR2. */
+	float distortion_calibration[8];
+
+	/* Timing data */
+	int timestamp_samples;
+
+	//timepoint_ns last_imu_vts_ns;
+	//timepoint_ns last_slam_vts_ns;
+	//timepoint_ns system_zero_ns;
+	//timepoint_ns last_imu_ns;
+
+	//time_duration_ns hw2mono_vts;
+	//time_duration_ns hw2mono_imu;
+
+	/* Tracking state */
+	struct m_relation_history* slam_relation_history;
+	struct m_ff_vec3_f32* ff_gyro;
+
+	/* Eye State */
+	bool eye_feature_enabled;
+	bool face_feature_enabled;
+
+	struct psvr2_et_data et_data;
+};
+
+static inline struct psvr2_hmd* psvr2_hmd(struct xrt_device* xdev)
+{
+	return (struct psvr2_hmd*)xdev;
+}
+
+enum psvr2_hmd_input_name
+{
+	PSVR2_HMD_INPUT_HEAD_POSE,
+	PSVR2_HMD_INPUT_FUNCTION_BUTTON,
+	PSVR2_HMD_INPUT_EYE_GAZE_POSE,
+	PSVR2_HMD_INPUT_FB_FACE_TRACKING2_SOCIAL,
+	PSVR2_HMD_INPUT_HTC_EYE_FACE_TRACKING,
+	PSVR2_HMD_INPUT_ANDROID_FACE_TRACKING,
+	PSVR2_HMD_INPUT_COUNT,
+};
+
+void psvr2_compute_distortion_asymmetric(float* calibration, struct xrt_uv_triplet* distCoords, int eEye, float fU, float fV);
+
+bool psvr2_usb_xfer_continue(struct libusb_transfer* xfer, const char* type);
+
+bool send_psvr2_control(struct psvr2_hmd* hmd, uint16_t report_id, uint8_t subcmd, uint8_t* pkt_data, uint32_t pkt_len);
+
+void psvr2_free_et_data(struct psvr2_hmd* hmd);
+
+int psvr2_start_gaze_tracking(struct psvr2_hmd* hmd);
+
+//xrt_result_t psvr2_get_face_tracking(struct xrt_device* xdev, enum xrt_input_name facial_expression_type,
+//	int64_t at_timestamp_ns, struct xrt_facial_expression_set* out_value);
+
 
 #endif
